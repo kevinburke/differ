7596500234bb9fd09fcd94141dab52168ce40673
Kevin Burke <kev@inburke.com>
2018-07-19 14:31:35 -0700

    Show first few lines of the git diff
    This should help figure out what's going wrong when differ reports
    that there's a problem. We only show the first 20 lines of a diff.

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"os"
@@ -21,17 +23,39 @@ func init() {
 	}
 }
 
+func getGitDiff(ctx context.Context) string {
+	diffBuf := new(bytes.Buffer)
+	diffCmd := exec.CommandContext(ctx, "git", "diff", "--no-color")
+	diffCmd.Stdout = diffBuf
+	diffCmd.Stderr = diffBuf
+	if diffErr := diffCmd.Run(); diffErr != nil {
+		return ""
+	}
+	if diffBuf.Len() == 0 {
+		return ""
+	}
+	bs := bufio.NewScanner(diffBuf)
+	diffOutput := strings.Builder{}
+	for i := 0; i < 20 && bs.Scan(); i++ {
+		diffOutput.Write(bs.Bytes())
+		diffOutput.WriteByte('\n')
+	}
+	return "\nFirst few lines of the git diff:\n" + diffOutput.String()
+}
+
 func main() {
 	flag.Parse()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	if len(os.Args) <= 1 {
 		flag.Usage()
 		os.Exit(2)
 	}
 	var cmd *exec.Cmd
 	if len(os.Args) == 2 {
-		cmd = exec.Command(os.Args[1])
+		cmd = exec.CommandContext(ctx, os.Args[1])
 	} else {
-		cmd = exec.Command(os.Args[1], os.Args[2:]...)
+		cmd = exec.CommandContext(ctx, os.Args[1], os.Args[2:]...)
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -42,7 +66,7 @@ func main() {
 		// just do 2
 		os.Exit(2)
 	}
-	gitCmd := exec.Command("git", "status", "--porcelain")
+	gitCmd := exec.CommandContext(ctx, "git", "status", "--porcelain")
 	buf := new(bytes.Buffer)
 	gitCmd.Stdout = buf
 	gitCmd.Stderr = buf
@@ -52,12 +76,13 @@ func main() {
 		os.Exit(2)
 	}
 	if buf.Len() > 0 {
+		diff := getGitDiff(ctx)
 		fmt.Fprintf(os.Stderr, `
 Untracked or modified files present after running '%s':
 
-%s
+%s%s
 The command should not generate a diff. Please fix the problem and try again.
-`, strings.Join(os.Args[1:], " "), buf.String())
+`, strings.Join(os.Args[1:], " "), buf.String(), diff)
 		os.Exit(2)
 	}
 }
